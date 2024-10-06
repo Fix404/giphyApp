59e3b45255fa79f157e1b73e16daa5058130f07c
Maximiliano Niscola <niscolamaximiliano@gmail.com>
2024-10-06 19:54:45 -0300

    giphy completo

---
+++ giphyApp/src/App.tsx
@@ -0,0 +1,14 @@
+import { GifList } from "./components/GifList/GifList"
+import { NavBar } from "./components/ui/NavBar/NavBar"
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <GifList />
+    </>
+  )
+}
+
+export default App
